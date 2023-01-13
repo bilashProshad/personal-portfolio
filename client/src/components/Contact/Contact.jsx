db6b776ebfe4f990584db4298109ed1019d6c7f2
Bilash Prosad <pbilash64@gmail.com>
2023-01-13 22:38:23 +0600

    fixed api releted problem

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -55,7 +55,7 @@ const Contact = () => {
       return;
     }
 
-    const response = await fetch(`/contact`, {
+    const response = await fetch(`/api/contact`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",

36d9fb42436ffd23f01b1cb645906177fb093c38
Bilash Prosad <pbilash64@gmail.com>
2023-12-21 21:23:19 +0600

    cursor style added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.scss";
 import Contact from "./components/Contact/Contact";
+import Cursor from "./components/Cursor/Cursor";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
@@ -10,6 +11,7 @@ import Skills from "./components/Skills/Skills";
 function App() {
   return (
     <div className="App">
+      <Cursor />
       <Navbar />
       <Home />
       <Skills />

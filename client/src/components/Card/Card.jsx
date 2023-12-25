cb63de85f542c4cd498f3e7cc27ad994f066f485
Bilash Prosad <pbilash64@gmail.com>
2023-12-26 03:00:03 +0600

    basic animation added

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -8,24 +8,11 @@ const Card = ({
   className,
   defaultContent = true,
   children,
-  delay = 0,
-  x = "-100%",
+
   ...rest
 }) => {
   return defaultContent ? (
-    <motion.div
-      className={`defaultCard ${className}`}
-      {...rest}
-      initial={{
-        x: x,
-        opacity: 0,
-      }}
-      whileInView={{
-        x: 0,
-        opacity: 1,
-      }}
-      transition={{ duration: 0.3, delay: delay }}
-    >
+    <motion.div className={`defaultCard ${className}`} {...rest}>
       <img src={icon} alt={title} />
 
       <h3>{title}</h3>
--- client/src/components/Services/Services.jsx
+++ client/src/components/Services/Services.jsx
@@ -2,6 +2,22 @@ import { useSetActiveLink } from "../../hooks/useSetActiveLink";
 import Card from "../Card/Card";
 import SectionHeader from "../SectionHeader/SectionHeader";
 import "./Services.scss";
+import { motion } from "framer-motion";
+
+const cardVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.1,
+      staggerChildren: 0.2,
+    },
+  },
+};
 
 const Services = ({ setActiveLink }) => {
   const sectionRef = useSetActiveLink({
@@ -18,29 +34,31 @@ const Services = ({ setActiveLink }) => {
             description="As a web developer, I offer a wide range of services:"
           />
         </div>
-        <div className="bottom">
+        <motion.div
+          className="bottom"
+          variants={cardVariants}
+          initial="initial"
+          whileInView={"animate"}
+        >
           <Card
             title="Website Design"
             icon={`/images/icons/web-design.svg`}
             text="I can help you create a visually appealing, user-friendly website that reflects your brand and meets the needs of your target audience."
-            delay={window.outerWidth <= 570 ? 0 : 0.2}
-            x="-100%"
+            variants={cardVariants}
           />
           <Card
             title="Website Development"
             icon={`/images/icons/web-dev.svg`}
             text="From custom coding to integration with third-party platforms, I have the skills and experience to bring your website to life."
-            delay={window.outerWidth <= 570 ? 0 : 0.5}
-            x={window.outerWidth >= 820 ? "-200%" : "-100%"}
+            variants={cardVariants}
           />
           <Card
             title="Website Maintenance"
             icon={`/images/icons/web-maintenance.svg`}
             text="I can help you keep your website up-to-date and secure with regular updates, backups, and monitoring."
-            delay={window.outerWidth <= 570 ? 0 : 0.8}
-            x={window.outerWidth <= 820 ? "-100%" : "-300%"}
+            variants={cardVariants}
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   );

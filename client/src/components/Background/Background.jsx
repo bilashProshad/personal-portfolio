4e7726ba8e5e566adb737cb6b923100ac27db831
Bilash Prosad <pbilash64@gmail.com>
2023-12-28 20:52:24 +0600

    changed background

--- client/src/components/Background/Background.jsx
+++ client/src/components/Background/Background.jsx
@@ -1,10 +1,14 @@
 import "./Background.scss";
 import { createPortal } from "react-dom";
+// import videoBG from "../../assets/bgv3.mp4";
 
 const Background = () => {
   return createPortal(
     <div className="background">
-      <div class="boxs">
+      {/* <video autoPlay loop muted playsInline className="clip">
+        <source src={videoBG} type="video/mp4" />
+      </video> */}
+      {/* <div class="boxs">
         <span></span>
         <span></span>
         <span></span>
@@ -15,7 +19,7 @@ const Background = () => {
         <span></span>
         <span></span>
         <span></span>
-      </div>
+      </div> */}
     </div>,
     document.getElementById("background")
   );
